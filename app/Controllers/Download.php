c41b11d407ae82376610cebb3cd6254d0d086489
Mark Whalley <mark@ipx.co.uk>
2022-01-01 23:46:09 +0000

    agent detecteion change

--- app/Controllers/Download.php
+++ app/Controllers/Download.php
@@ -19,11 +19,15 @@ class Download extends Controller
         // Fill-in standard URLs
         $releaseList = $mmexConfig->releaseList;
         $i = 0;
-        foreach ($releaseList as $release) {
-            foreach ($release['contents'] as $releaseBuild => $releaseURL) {
-                if (empty($releaseURL)) {
+        foreach ($releaseList as $release) 
+        {
+            foreach ($release['contents'] as $releaseBuild => $releaseURL) 
+            {
+                if (empty($releaseURL)) 
+                {
                     $build = "";
-                    switch ($releaseBuild) {
+                    switch ($releaseBuild) 
+                    {
                         case "mac":
                             $build = "https://github.com/moneymanagerex/moneymanagerex/releases/download/v${release['version']}/mmex-${release['version']}-Darwin.dmg";
                             break;
@@ -33,10 +37,10 @@ class Download extends Controller
                         case "win32":
                             $build = "https://github.com/moneymanagerex/moneymanagerex/releases/download/v${release['version']}/mmex-${release['version']}-win32.exe";
                             break;
-                        case "win64port":
+                        case "win64-port":
                             $build = "https://github.com/moneymanagerex/moneymanagerex/releases/download/v${release['version']}/mmex-${release['version']}-win64-portable.zip";
                             break;
-                        case "win32port":
+                        case "win32-port":
                             $build = "https://github.com/moneymanagerex/moneymanagerex/releases/download/v${release['version']}/mmex-${release['version']}-win32-portable.zip";
                             break;
                     }
@@ -46,58 +50,50 @@ class Download extends Controller
             $i++;
         };
 
-        $downloads = array(); 
         $latestRelease = $releaseList[0];
-        $latestVersion = $latestRelease['version'];
-        $agent = $this->request->getUserAgent();
-        $platformVersion = $agent->getPlatform();
-        if (!strcmp($platformVersion,"Mac OS X")) {
-            $osVersion = "Mac";
+
+        // Determine the platform the user is using
+        $osName = "";
+        $agent = $_SERVER['HTTP_USER_AGENT'];
+        if (strpos($agent,"Win"))
+            if (strpos($agent,"x64"))
+                $osName = "win64";
+            else
+                $osName = "win32";
+        if (strpos($agent,"Mac")) $osName = "mac";
+        if (strpos($agent,"Linux")) $osName = "linux";   
+        if (strpos($agent,"Android")) $osName = "android";
+        if (strpos($agent,"like Mac")) $osName = "ios"; 
+
+        // Find the applicable recommended builds for download
+        $downloads = array(); 
+        foreach($latestRelease['contents'] as $releaseBuild => $releaseURL)  
+        {            
+            $releaseNameSplit = explode('-',$releaseBuild);         
+            if (!strcmp($osName,$releaseNameSplit[0]))
+            {
+                $button = [ 
+                    'name' => $mmexConfig->releaseNames[$releaseBuild],
+                    'link' => $releaseURL
+                ];
+                $downloads[] = $button;
+            }
+        }
+    
+        // If nothing found for the platform then just add an 'unknown' button
+        if (empty($downloads))
+        {
             $button = [ 
-                'name' => "$osVersion (v$latestVersion)",
-                'link' => $latestRelease['contents']['mac']
-            ];
-            $downloads[] = $button;
-        } else if (!strcmp($platformVersion,"Windows 7")
-                || !strcmp($platformVersion,"Windows 8")
-                || !strcmp($platformVersion,"Windows 8.1")
-                || !strcmp($platformVersion,"Windows 10")) {
-            $osVersion = "Windows";
-            $button = [ 
-                'name' => "$osVersion 64-bit (v$latestVersion)",
-                'link' => $latestRelease['contents']['win64']
-            ];
-            $downloads[] = $button;
-            $button = [ 
-                'name' => "$osVersion 32-bit (v$latestVersion)",
-                'link' => $latestRelease['contents']['win32']
-            ];
-            $downloads[] = $button;
-        } else if (!strcmp($platformVersion,"Linux")
-                || !strcmp($platformVersion,"Debian")
-                || !strcmp($platformVersion,"GNU/Linux")
-                || !strcmp($platformVersion,"Unknown Unix OS")) {
-            $osVersion = "Linux";
-            $button = [ 
-                'name' => "$osVersion (Select from Linux builds below)",
-                'link' => ""
-            ];
-            $downloads[] = $button;
-        } else {
-            $osVersion = "";
-            $button = [ 
-                'name' => "Unknown (Select from builds below)",
-                'link' => ""
+                'name' => "Sorry, Platform Not Identified",
+                'link' => "#"
             ];
             $downloads[] = $button;
         }
 
-        $data['latestVersion'] = $latestVersion;
-        $data['platformVersion'] = $platformVersion;
-        $data['platformType'] = $osVersion;
         $data['downloads'] = $downloads;
         $data['releaseNames'] = $mmexConfig->releaseNames;
         $data['releaseList'] = $releaseList;
+        $data['latestVersion'] = $latestRelease['version'];
 
         echo view('templates/header', $data);
         echo view('templates/navigation', $data);
--- app/Views/downloads/index.php
+++ app/Views/downloads/index.php
@@ -4,13 +4,13 @@
         <div class="row justify-content-center">
             <div class="col-lg-12">
                 <div class="text-center my-1">
-                    <h1 class="fw-bolder text-white mb-3">Download the latest recommended build</h1>
+                    <h1 class="fw-bolder text-white mb-3">Download the latest v<?php echo $latestVersion ?> recommended build</h1>
                     <p class="lead fw-normal text-muted mb-4">Either download the latest recommended version for your platform or select from the available versions below.</p>
                     <div>
 <?php foreach($downloads as $download) { ?>
-                        <a class="btn btn-primary btn-lg" href="<?php echo $download['link'] ?>"><i class="fas fa-download"></i>&nbsp;<?php echo $download['name'] ?></a>
+                        <a class="btn btn-primary btn-lg mb-1" href="<?php echo $download['link'] ?>"><i class="fas fa-download"></i>&nbsp;<?php echo $download['name'] ?></a>
 <?php } ?>
-                        <a class="btn btn-outline-light btn-lg" href="https://sourceforge.net/projects/moneymanagerex/files/v<?php echo $latestVersion ?>/">Alternate download</a>
+                        <a class="btn btn-outline-light btn-lg mb-1" href="https://sourceforge.net/projects/moneymanagerex/files/v<?php echo $latestVersion ?>/">Alternate download</a>
                     </div>
                 </div>
             </div>
